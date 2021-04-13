61ede487381fddc22ef9945fd60c1df0d6a92415
ngochuykm <fromhd.1990@gmail.com>
2021-04-13 15:07:57 +0700

    upcode

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class _MyHomePageState extends State<MyHomePage> {
   File _video;
   File _cameraVideo;
   double _videoinfo;
-  List<int> timeFrame = [0,1000];
+  List<int> timeFrame = [0, 1000];
 
   ImagePicker picker = ImagePicker();
 
@@ -68,35 +68,37 @@ class _MyHomePageState extends State<MyHomePage> {
         _cameraVideoPlayerController.play();
       });
   }
+
   _seekvideo(time) async {
     print(" seek video ::: " + _video.path);
     setState(() {
-      if(_videoPlayerController!=null ){
+      if (_videoPlayerController != null) {
         _videoPlayerController.seekTo(Duration(milliseconds: time));
         _videoPlayerController.pause();
-        }
+      }
     });
   }
+
   _playvideo() async {
     print(" play video ::: ");
     setState(() {
       _videoPlayerController.play();
     });
   }
+
   _getvideoinfo(path) async {
-    if(_video!=null){
+    if (_video != null) {
       final videoInfo = FlutterVideoInfo();
       final videoinfo = await videoInfo.getVideoInfo(path);
-      setState(
-        ()  {
-          _videoinfo = videoinfo.duration;
-        }
-      );
+      setState(() {
+        _videoinfo = videoinfo.duration;
+      });
       print(videoinfo.duration);
       print("data from get video info ::: " + _videoinfo.toString());
       return videoinfo.duration;
     }
   }
+
   @override
   Widget build(BuildContext context) {
     print("duration :  " + _videoinfo.toString());
@@ -111,12 +113,10 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Column(
               children: <Widget>[
                 if (_video != null)
-                  _videoPlayerController.value.initialized
-                      ? AspectRatio(
-                          aspectRatio: _videoPlayerController.value.aspectRatio,
-                          child: VideoPlayer(_videoPlayerController),
-                        )
-                      : Container()
+                  AspectRatio(
+                    aspectRatio: _videoPlayerController.value.aspectRatio,
+                    child: VideoPlayer(_videoPlayerController),
+                  )
                 else
                   Text(
                     "Click on Pick Video to select video",
@@ -129,24 +129,22 @@ class _MyHomePageState extends State<MyHomePage> {
                   child: Text("Pick Video From Gallery"),
                 ),
                 if (_cameraVideo != null)
-                  _cameraVideoPlayerController.value.initialized
-                      ? AspectRatio(
-                          aspectRatio:
-                              _cameraVideoPlayerController.value.aspectRatio,
-                          child: Column(
-                                    children: [
-                                      VideoPlayer(_cameraVideoPlayerController),
-                                      if(_video!=null) ValueListenableBuilder(
-                                        valueListenable: _videoPlayerController,
-                                        builder: (context, VideoPlayerValue value, child) {
-                                          //Do Something with the value.
-                                          return Text(value.position.toString());
-                                        },
-                                      ),
-                                    ],
-                                  ),
-                        )
-                      : Container()
+                  AspectRatio(
+                    aspectRatio: _cameraVideoPlayerController.value.aspectRatio,
+                    child: Column(
+                      children: [
+                        VideoPlayer(_cameraVideoPlayerController),
+                        if (_video != null)
+                          ValueListenableBuilder(
+                            valueListenable: _videoPlayerController,
+                            builder: (context, VideoPlayerValue value, child) {
+                              //Do Something with the value.
+                              return Text(value.position.toString());
+                            },
+                          ),
+                      ],
+                    ),
+                  )
                 else
                   Text(
                     "Click on Pick Video to select video",
@@ -156,52 +154,52 @@ class _MyHomePageState extends State<MyHomePage> {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
                     RaisedButton(
-                    onPressed: () {
-                      _seekvideo(1500);
-                    },
-                    child: Text("Seek"),
+                      onPressed: () {
+                        _seekvideo(1500);
+                      },
+                      child: Text("Seek"),
                     ),
                     RaisedButton(
-                    onPressed: () {
-                      _playvideo();
-                    },
-                    child: Text("Play"),
+                      onPressed: () {
+                        _playvideo();
+                      },
+                      child: Text("Play"),
                     ),
                     RaisedButton(
-                    onPressed: () {
-                      _getvideoinfo(_video.path);
-                    },
-                    child: Text("Info"),
+                      onPressed: () {
+                        _getvideoinfo(_video.path);
+                      },
+                      child: Text("Info"),
                     ),
                   ],
                 ),
-                Padding(padding: EdgeInsets.only(top:10)),
-                if(_video!=null)
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                  children: [
-                    FlatButton(
-                      onPressed:() => _seekvideo(0),
-                      child: RenderFrame(_video.path,0000),
-                      ),
-                    FlatButton(
-                      onPressed:() => _seekvideo(1000),
-                      child: RenderFrame(_video.path,1000),
-                      ),
-                    FlatButton(
-                      onPressed:() => _seekvideo(2000),
-                      child: RenderFrame(_video.path,2000),
-                      ),
-                    FlatButton(
-                      onPressed:() => _seekvideo(3000),
-                      child: RenderFrame(_video.path,3000),
-                      ),
-                  ],
-                ),)
-                ,
+                Padding(padding: EdgeInsets.only(top: 10)),
+                if (_video != null)
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        FlatButton(
+                          onPressed: () => _seekvideo(0),
+                          child: RenderFrame(_video.path, 0000),
+                        ),
+                        FlatButton(
+                          onPressed: () => _seekvideo(1000),
+                          child: RenderFrame(_video.path, 1000),
+                        ),
+                        FlatButton(
+                          onPressed: () => _seekvideo(2000),
+                          child: RenderFrame(_video.path, 2000),
+                        ),
+                        FlatButton(
+                          onPressed: () => _seekvideo(3000),
+                          child: RenderFrame(_video.path, 3000),
+                        ),
+                      ],
+                    ),
+                  ),
                 Padding(
-                  padding: EdgeInsets.only(top:10),
+                  padding: EdgeInsets.only(top: 10),
                 ),
                 Padding(padding: EdgeInsets.only(top: 10)),
                 Text(_videoinfo.toString()),
@@ -220,37 +218,40 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class RenderFrame extends StatelessWidget{
+class RenderFrame extends StatelessWidget {
   final String path;
   final int time;
   _getframe(time) async {
     print(" get frame ::: ");
     final fileName = await VideoThumbnail.thumbnailData(
-      video: path,
-      imageFormat: ImageFormat.WEBP,
-      maxHeight: 64, // specify the height of the thumbnail, let the width auto-scaled to keep the source aspect ratio
-      quality: 75,
-      timeMs: time
-    );
+        video: path,
+        imageFormat: ImageFormat.WEBP,
+        maxHeight:
+            64, // specify the height of the thumbnail, let the width auto-scaled to keep the source aspect ratio
+        quality: 75,
+        timeMs: time);
     return fileName;
   }
 
-  RenderFrame(this.path,this.time);
-  Widget build(BuildContext context){
+  RenderFrame(this.path, this.time);
+  Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.only(left:10,right:10),
-      child: FutureBuilder<dynamic>(
-        future: _getframe(time),
-        builder: (context, snapot){
-          return Column(
-            children: [
-              if(snapot.data!=null) Image.memory(snapot.data),
-              Padding(padding: EdgeInsets.only(top: 3)),
-              Text("Form : " + (time/1000).toString() + "s",style: TextStyle(color: Colors.blueAccent, fontSize: 10,fontFamily: 'Raleway'))
-            ],
-          );
-        },
-      )
-    );
+        padding: EdgeInsets.only(left: 10, right: 10),
+        child: FutureBuilder<dynamic>(
+          future: _getframe(time),
+          builder: (context, snapot) {
+            return Column(
+              children: [
+                if (snapot.data != null) Image.memory(snapot.data),
+                Padding(padding: EdgeInsets.only(top: 3)),
+                Text("Form : " + (time / 1000).toString() + "s",
+                    style: TextStyle(
+                        color: Colors.blueAccent,
+                        fontSize: 10,
+                        fontFamily: 'Raleway'))
+              ],
+            );
+          },
+        ));
   }
 }
